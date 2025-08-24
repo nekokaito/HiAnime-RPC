9eac5c9c0ffc903cdc9eebbb9094f20cfd476ef6
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-08-24 19:34:04 +0600

    static test

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hi Anime");
+const rpc = require("discord-rpc");
+
+const clientId = "1409166870069772328";
+
+const client = new rpc.Client({ transport: "ipc" });
+
+client.on("ready", () => {
+  console.log("Rich Presence is active!");
+
+  client.setActivity({
+    details: "Watching Anime",
+    state: "Episode 1",
+    largeImageKey: "hianime",
+    smallImageKey: "play",
+    buttons: [{ label: "Watch on HiAnime", url: "https://hianime.to" }],
+  });
+});
+
+client.login({ clientId }).catch(console.error);
